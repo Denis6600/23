21651c5d499113c752f29b46bb5e894e35c59b82
Denis Yalymov <>
2023-04-02 15:32:27 +0300

    23

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число N: ");
+int num = int.Parse(Console.ReadLine());
+
+
+
+for (int i = 1; i <= num; i++)
+{
+  double pow = Math.Pow(i,3);
+  Console.WriteLine($"{pow}");
+}
